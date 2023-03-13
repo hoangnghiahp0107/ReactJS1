b1d93f7fddf00274996203569041e9ae6a1a32dd
hoangnghia0107 <hoangnghiahp0107@gmail.com>
2023-03-13 22:38:11 +0700

    build:hoangnghia

---
+++ src/BaiTapThucHanhLayout/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Banner from "./Banner";
+import Item from "./Item";
+import Footer from "./Footer"; 
+function Home(){
+    return (
+    <div>
+      <Header/>
+      <Banner/>
+      <Item/>
+      <Footer/>
+    </div>
+    );
+}
+
+export default Home;
